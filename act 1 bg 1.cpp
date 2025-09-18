a3418cb292902d33ca339f3b327d89743c9c09c2
faizabdurrahman-hash <faizabdurrahman@mail.ugm.ac.id>
2025-09-18 14:56:29 +0700

    Add files via upload

---
+++ act 1 bg 1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int A=2, B=2, C=3;
+	A = 2*B;
+	C = A+B;
+	cout<<"nilai A: "<<A<<" & C:"<<C;
+}
---
+++ act 1 bg 2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a=2,b=3, c=4;
+	c = a + b;
+	a = 2 * b;
+	cout<<"nilai a: "<<a<<" & c: "<<c;
+}
