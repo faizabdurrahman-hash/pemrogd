0714e6b09b8a3c3f0afd26117e584e5964d4fea6
faizabdurrahman-hash <faizabdurrahman@mail.ugm.ac.id>
2025-10-08 11:03:51 +0700

    Latihan: Array 1D

--- latihan 1.cpp
+++ latihan 1.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
-int main (){
-	int n;
-	cout<<"nilai";
-	cin>>n;
-	
-	for (int i = 0; i < n; i++){
-		int arr[i];
-		cin >> arr[i];
-	}
-		int arr[n];
-	    int mx = arr[0];
-		for (int i = 1; i < n; i++){
-		
-			if(arr[i] > mx){
-				mx = arr[i];
-				cout << mx << "\n";
-			}
-		}
 
+int main() {
+    int n;
+    cout << "Masukkan jumlah nilai yang ingin dikeluarkan: ";
+    cin >> n;
+
+    int arr[n]; // buat array dengan ukuran n
+
+    cout << "Masukkan nilai:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int mx = arr[0]; // nilai maksimum awal
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > mx) {
+            mx = arr[i];
+        }
+    }
+
+    cout << "Nilai maksimum adalah: " << mx << endl;
+
+    return 0;
 }
-
