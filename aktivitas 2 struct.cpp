97360b58685e6c2665a3be8b33c3660863bf8862
faizabdurrahman-hash <faizabdurrahman@mail.ugm.ac.id>
2025-10-23 14:54:11 +0700

    Add files via upload

---
+++ aktivitas 2 struct.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+
+struct Mahasiswa {
+string nama;
+int umur;
+string nik; 
+};
+
+int main() {
+const int jumlahMahasiswa = 1; 
+Mahasiswa mhs[jumlahMahasiswa]; 
+
+// Mengisi data mahasiswa
+for (int i = 0; i < jumlahMahasiswa; i++) {
+cout << "Masukkan nama mahasiswa ke-" << i + 1 << ": ";
+getline(cin, mhs[i].nama); 
+
+cout << "Masukkan umur: ";
+cin >> mhs[i].umur;
+cin.ignore(); // Mengabaikan newline leftover dari cin
+cout << "Masukkan NIK: ";
+
+
+
+getline(cin, mhs[i].nik); 
+
+cout << endl; 
+}
+// Menampilkan data mahasiswa
+cout << "\nData Mahasiswa:" << endl;
+for (int i = 0; i < jumlahMahasiswa; i++) {
+cout << "Mahasiswa " << i + 1 << ":" << endl;
+cout << "Nama: " << mhs[i].nama << endl;
+cout << "Umur: " << mhs[i].umur << endl;
+cout << "NIK: " << mhs[i].nik << endl;
+cout << endl;
+}
+return 0;
+}
---
+++ aktivitas 4 struct.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+struct Mahasiswa {
+string nama;
+int umur;
+string nik;
+string jabatan;
+};
+
+int main() {
+const int jumlahMahasiswa = 100;
+Mahasiswa mhs[jumlahMahasiswa];
+int count = 0;
+ifstream inputFile("mahasiswa.txt");
+if (!inputFile) {
+cerr << "Tidak dapat membuka file!" << endl;
+return 1;
+}
+string line;
+while (getline(inputFile, line) && count < jumlahMahasiswa) {
+
+
+
+stringstream ss(line);
+ss >> mhs[count].nama >> mhs[count].umur >> mhs[count].nik >> mhs[count].jabatan;
+count++;
+}
+inputFile.close();
+cout << "nomer opsi: " << endl << "1. Menampilkan semua data karyawan." << endl;
+cout << "2. Menambahkan karyawan baru." << endl << "3. Mengedit data karyawan berdasarkan NIK." << endl;
+cout << "masukan no. opsi: " ;
+int opsi = 0;
+cin >> opsi;
+//for (opsi == 1; || opsi == 2; || opsi == 3;){
+
+if(opsi == 1){
+
+cout << "Data Mahasiswa:" << endl;
+for (int i = 0; i < count; i++) {
+cout << "Mahasiswa " << i + 1 << ":" << endl;
+cout << "Nama: " << mhs[i].nama << endl;
+cout << "Umur: " << mhs[i].umur << endl;
+cout << "NIK: " << mhs[i].nik << endl;
+cout << "Jabatan: " << mhs[i].jabatan << endl;
+cout << endl; // Baris kosong untuk pemisah
+}
+}else if(opsi == 2){
+int jumlahMhs = 0;
+cout << "maunya input brp karyawan(diatas 3): ";
+cin >> jumlahMhs;
+Mahasiswa mhs[jumlahMhs]; 
+
+// Mengisi data mahasiswa
+for (int i = 0; i < jumlahMhs; i++) {
+cout << "Masukkan nama mahasiswa ke-" << i + 1 << ": ";
+cin >> mhs[i].nama ;
+//getline(cin, mhs[i].nama); 
+cout << endl;
+cout << "Masukkan umur: ";
+cin >> mhs[i].umur;
+cin.ignore(); // Mengabaikan newline leftover dari cin
+cout << "Masukkan NIK: ";
+
+
+
+
+getline(cin, mhs[i].nik); 
+
+cout << endl; 
+}
+}else if(opsi == 3){
+	cout << "edit" << endl;
+}
+//}
+//cout << "masukan no. opsi: " ;
+//cin >> opsi;
+// Menampilkan data mahasiswa
+cout << "\nData Mahasiswa:" << endl;
+for (int i = 0; i < jumlahMahasiswa; i++) {
+cout << "Mahasiswa " << i + 1 << ":" << endl;
+cout << "Nama: " << mhs[i].nama << endl;
+cout << "Umur: " << mhs[i].umur << endl;
+cout << "NIK: " << mhs[i].nik << endl;
+cout << endl;
+}
+
+return 0;
+}
